54685c10accd7624f3c5be202449636e2c589c8b
Haris-Rahat <har@technologies.dk>
2024-01-04 12:54:03 +0500

    removed @prefresh/vite
    The package is injected automatically at runtime

--- src/routes/attendance/components/userCard.tsx
+++ src/routes/attendance/components/userCard.tsx
@@ -19,24 +19,28 @@ const UserCard: FunctionComponent<{ user: Record<string, any> }> = ({
   }, []);
 
   return (
-    <div className={"w-1/5 bg-blue-500 m-4 rounded-lg p-4 flex flex-col justify-between"}>
+    <div
+      className={
+        "w-1/5 bg-blue-500 m-4 rounded-lg p-4 flex flex-col justify-between"
+      }
+    >
       <div>
-      <img
-        src={
-          "https://unsplash.com/photos/a-desert-landscape-with-mountains-in-the-distance-SGZ5DkDOoRo"
-          // `${fileUrl}${user.avatar}`
-        }
-        alt=""
-        className={"w-40 h-40 rounded-full mx-auto"}
-      />
-      <p
-        className={"text-center text-lg mt-6"}
-      >{`${user.firstName} ${user?.lastName}`}</p>
-      <p className={"text-center"}>{`${user?.jobTitle?.jobTitle}`}</p>
+        <img
+          src={
+            "https://unsplash.com/photos/a-desert-landscape-with-mountains-in-the-distance-SGZ5DkDOoRo"
+            // `${fileUrl}${user.avatar}`
+          }
+          alt="avatar"
+          className={"w-40 h-40 rounded-full mx-auto"}
+        />
+        <p
+          className={"text-center text-lg mt-6"}
+        >{`${user.firstName} ${user?.lastName}`}</p>
+        <p className={"text-center"}>{`${user?.jobTitle?.jobTitle}`}</p>
       </div>
       <div className={"flex-row mx-auto items-stretch w-full mt-8"}>
-      <button className={"text-white rounded w-[80%]"}>ClockIn</button>
-      <button className={"w-6"}>V</button>
+        <button className={"text-white rounded w-[80%]"}>ClockIn</button>
+        <button className={"w-6"}>V</button>
       </div>
     </div>
   );
